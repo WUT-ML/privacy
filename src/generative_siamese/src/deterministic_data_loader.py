Style fixes. KL div added.

src/generative_siamese/src/deterministic_data_loader.py
@@ -57,7 +57,8 @@ class TripletToyDataset(Dataset):
             # For "virtual" indexes 7200-14399 return first image of the given id
             # and an image of a different id
             label = 1
-            person_id, variant_id = divmod(index - self.N_IDENTITIES * self.TOTAL_N_VARIANTS + self.TOTAL_N_VARIANTS, self.TOTAL_N_VARIANTS)
+            person_id, variant_id = divmod(index - self.N_IDENTITIES * self.TOTAL_N_VARIANTS +
+                                           self.TOTAL_N_VARIANTS, self.TOTAL_N_VARIANTS)
             variant_1, variant_2 = self.variants[variant_id]
             others = list(set(np.arange(1, self.N_IDENTITIES + 1)) - set([person_id]))
             img_1 = self.get_img(person_id, variant_1)
@@ -91,9 +92,9 @@ class ToyDataset(Dataset):
 
         img_name = '{id:02d}_{var:02d}_'.format(id=person_id, var=variant_id)
         img_path = glob.glob(self.root_path + img_name + '*.png')[0]
+        rel_path = os.path.relpath(img_path, self.root_path)
 
-        return np.expand_dims(scipy.misc.imread(img_path, mode="L"), 2), os.path.relpath(img_path,
-                                                                               self.root_path)
+        return np.expand_dims(scipy.misc.imread(img_path, mode="L"), 2), rel_path
 
     def __getitem__(self, index):
         """Access item from dataset."""

src/generative_siamese/src/reference_data_loader.py
@@ -34,9 +34,11 @@ class FERGDataset(Dataset):
         img_name = self.filenames.iloc[index, 0]
         img_path = os.path.join(self.root_path, img_name)
         rel_path = os.path.relpath(img_path, self.root_path)
-        attr = FERGAttributes(rel_path)
+        attributes = FERGAttributes(rel_path)
+        attr = attributes.get_attr()
+        id = attributes.get_id()
 
-        return np.expand_dims(scipy.misc.imread(img_path, mode="L"), 2), attr.get_id(), attr.get_attr()
+        return np.expand_dims(scipy.misc.imread(img_path, mode="L"), 2), id, attr
 
     def __getitem__(self, index):
         """Access item from dataset."""
@@ -48,19 +50,24 @@ class FERGDataset(Dataset):
 
         return img_1, id, attr
 
+
 class FERGAttributes():
     """Class representing id and attributes for images from FERG dataset."""
 
-    dict_id = {'aia' : 0, 'bonnie' : 1, 'jules' : 2, 'malcolm' : 3, 'mery' : 4, 'ray' : 5}
-    dict_attr = {'anger' : 0, 'disgust' : 1, 'fear' : 2, 'joy' : 3, 'neutral' : 4, 'sadness' : 5, 'surprise' : 6}
+    dict_id = {'aia': 0, 'bonnie': 1, 'jules': 2, 'malcolm': 3, 'mery': 4, 'ray': 5}
+    dict_attr = {'anger': 0, 'disgust': 1, 'fear': 2, 'joy': 3, 'neutral': 4, 'sadness': 5,
+                 'surprise': 6}
 
     def __init__(self, path):
+        """Initialize object, use file path to read id and attribute."""
         id_key, attr_key = (path.split('/')[1]).split('_')
         self.id = FERGAttributes.dict_id[id_key]
         self.attr = FERGAttributes.dict_attr[attr_key]
 
     def get_attr(self):
+        """Get attribute."""
         return self.attr
 
     def get_id(self):
+        """Get id."""
         return self.id

src/generative_siamese/src/reference_vgan_main.py
@@ -9,6 +9,7 @@ from reference_data_loader import FERGDataset
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 import tensorboardX as tb
+import os
 
 # Set parameters
 image_size = 64
@@ -24,7 +25,7 @@ dataset_transform = transforms.Compose([
 ])
 
 # Prepare dataset loader
-dataset = FERGDataset(transform=dataset_transform, path="../../../../../../FERG_DB_256/")
+dataset = FERGDataset(transform=dataset_transform, path="../../../../FERG_DB_256/")
 data_loader = DataLoader(dataset=dataset,
                          batch_size=256,
                          num_workers=4,
@@ -32,14 +33,13 @@ data_loader = DataLoader(dataset=dataset,
                          drop_last=False)
 
 
-
 def label_reshape_1d(label, label_dim):
     """Reshape label to list of zeros and ones."""
     out = torch.FloatTensor(len(label)).zero_()
     for i, l in enumerate(label):
         out[i] = l
-    
     return out
+
 
 def to_variable(tensor):
     """Convert tensor to variable."""
@@ -63,7 +63,6 @@ step = 0
 # Learning process
 for epoch in range(n_epochs):
     print("Epoch number: ", epoch)
-    
 
     train_discriminator = True
 
@@ -79,7 +78,7 @@ for epoch in range(n_epochs):
         labels = to_variable(labels)
         attr = to_variable(attr)
 
-        #----------------------TRAIN DISCRIMINATOR----------------------------
+        # ----------------------TRAIN DISCRIMINATOR----------------------------
         if(train_discriminator):
 
             # Train Discriminator on real images
@@ -102,16 +101,16 @@ for epoch in range(n_epochs):
             fake_ids_onehot = torch.zeros(batch_size, n_ids)
             fake_ids_onehot.scatter_(1, fake_ids, 1)
             fake_ids_onehot = to_variable(fake_ids_onehot)
-        
-            fake_images = g(images, fake_ids_onehot)
+
+            fake_images, _, _ = g(images, fake_ids_onehot)
             output_fake, _, _ = d(fake_images)
             loss = nn.BCEWithLogitsLoss().cuda()
             target = Variable(torch.zeros(output_fake.size()), requires_grad=True).cuda()
             d_loss_fake = loss(output_fake, target)
 
             # Combine losses
-            d_loss = 0.25 * d_loss_real + 0.5 * d_loss_id  + 0.25 * d_loss_attr + 0.25 * d_loss_fake
-        
+            d_loss = 0.25 * d_loss_real + 0.5 * d_loss_id + 0.25 * d_loss_attr + 0.25 * d_loss_fake
+
             # Backpropagation for discriminator
             d.zero_grad()
             g.zero_grad()
@@ -122,11 +121,12 @@ for epoch in range(n_epochs):
             tb_writer.add_scalar('discriminator_loss', d_loss.data[0], step)
 
             # Train generator twice as often as discriminator
-            train_discriminator = not train_discriminator
+            train_discriminator = False
 
+        else:
+            train_discriminator = True
 
-
-        #----------------------TRAIN GENERATOR----------------------------
+        # ----------------------TRAIN GENERATOR----------------------------
 
         # Train Generator
         batch_size = images.size(0)
@@ -136,22 +136,25 @@ for epoch in range(n_epochs):
         fake_ids_onehot = to_variable(fake_ids_onehot)
 
         # Generate fake images with controlled ids
-        fake_images = g(images, fake_ids_onehot)
+        fake_images, mu, logvar = g(images, fake_ids_onehot)
         output_fake, output_id, output_attr = d(fake_images)
 
-        # Train Generator to fool fake/real Discriminator 
+        # Train Generator to fool fake/real Discriminator
         loss = nn.BCEWithLogitsLoss().cuda()
         target = Variable(torch.ones(output_fake.size()), requires_grad=True).cuda()
         g_loss_fake = loss(output_fake, target)
-        
+
         # Train Generator to fool id Discriminator
         g_loss_id = F.cross_entropy(output_id, to_variable(fake_ids.long().squeeze()))
 
         # Train Generator to fool attr Discriminator
         g_loss_attr = F.cross_entropy(output_attr, attr.long())
 
-        # Combine losses 
-        g_loss = 0.11 * g_loss_fake + 0.6 * g_loss_id  + 0.29 * g_loss_attr
+        # Compute Kullback-Leibler divergence
+        kld_loss = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+
+        # Combine losses
+        g_loss = 0.108 * g_loss_fake + 0.6 * g_loss_id + 0.29 * g_loss_attr + 0.002 * kld_loss
 
         # Backpropagation for generator
         g.zero_grad()
@@ -163,7 +166,7 @@ for epoch in range(n_epochs):
         step += 1
 
     # At the end of each tenth epoch save generator and discriminator to file
-    if( ( epoch + 1 ) % 10 == 0):
+    if((epoch + 1) % 10 == 0):
         g_path = os.path.join(os.getcwd(), 'generator-%d.pkl' % (epoch+1))
         torch.save(g.state_dict(), g_path)
         d_path = os.path.join(os.getcwd(), 'discriminator-%d.pkl' % (epoch+1))

src/generative_siamese/src/reference_vgan_model.py
@@ -1,4 +1,4 @@
-"""Model of Privacy-Preserving Representation-Learning Variational Generative Adversarial Network."""
+"""Privacy-Preserving Representation-Learning Variational Generative Adversarial Network model."""
 
 import torch
 import torch.nn as nn
@@ -47,12 +47,12 @@ class Generator(nn.Module):
         c2 = F.leaky_relu(self.conv2_bn(self.conv2(c1)), 0.2)
         c3 = F.leaky_relu(self.conv3_bn(self.conv3(c2)), 0.2)
         c4 = F.leaky_relu(self.conv4_bn(self.conv4(c3)), 0.2)
-        fc11 = nn.Linear(c4.shape[1] * c4.shape[2] * c4.shape[3], 128).cuda() # mu layer
-        fc12 = nn.Linear(c4.shape[1] * c4.shape[2] * c4.shape[3], 128).cuda() # logvar layer
+        fc11 = nn.Linear(c4.shape[1] * c4.shape[2] * c4.shape[3], 128).cuda()  # mu layer
+        fc12 = nn.Linear(c4.shape[1] * c4.shape[2] * c4.shape[3], 128).cuda()  # logvar layer
 
         mu = fc11(c4.view(c4.size(0), -1))
         logvar = fc12(c4.view(c4.size(0), -1))
-        return mu, logvar        
+        return mu, logvar
 
     def decode(self, z, label):
         """Decode image from latent vector."""
@@ -66,18 +66,18 @@ class Generator(nn.Module):
         return decoder_out
 
     def reparameterize(self, mu, logvar):
-        """"z = mean + eps * sigma where eps is sampled from N(0, 1)."""
+        """Z = mean + eps * sigma where eps is sampled from N(0, 1)."""
         eps = Variable(torch.randn(mu.size(0), mu.size(1)))
         eps = eps.cuda()
         z = mu + eps * torch.exp(logvar/2)    # 2 for convert var to std
         return z
 
-
     def forward(self, input, label):
         """Define the computation performed at every call."""
         mu, logvar = self.encode(input)
         z = self.reparameterize(mu, logvar)
-        return self.decode(z, label)
+        return self.decode(z, label), mu, logvar
+
 
 class Discriminator(nn.Module):
     """Discriminator (detective) neural network."""
@@ -122,13 +122,13 @@ class Discriminator(nn.Module):
         discriminator_out_identity = fc_identity(discriminator_out_common)
         discriminator_out_attribute = fc_attribute(discriminator_out_common)
 
+        return (discriminator_out_fake,
+                self.softmax_identity(discriminator_out_identity),
+                self.softmax_attribute(discriminator_out_attribute))
 
-        return (discriminator_out_fake, self.softmax_identity(discriminator_out_identity), self.softmax_attribute(discriminator_out_attribute))
-        
 
 def normal_init(m, mean, std):
     """Initialize conv layer weights using normal distribution."""
     if isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Conv2d):
         m.weight.data.normal_(mean, std)
         m.bias.data.zero_()
-